dcb2d368e8abf0f2aa38ce82a76fe25b6131d52c
Gustavo Targino <luizgustavotargino23004@gmail.com>
2025-07-23 11:26:03 -0300

    Funcionalidade remoção logica + buscar

--- app/controllers/Produtos.java
+++ app/controllers/Produtos.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import model.Categoria;
 import model.Produto;
+import model.Status;
 import play.mvc.Controller;
 
 public class Produtos extends Controller{
@@ -22,18 +23,31 @@ public class Produtos extends Controller{
 	
 	public static void salvar(Produto produto) {
 		produto.save();
-		listar();	
+		listar(null);	
 	}
 	
-	public static void listar() {
-		List<Produto> listaDeProdutos = Produto.findAll();
-		render(listaDeProdutos);
+	public static void listar(String termo) {
+		List<Produto> listaDeProdutos = null;
+		
+		if (termo == null) {
+			listaDeProdutos = Produto.find("status <> ?1", Status.INATIVO).fetch();	
+		} else {
+			listaDeProdutos = Produto.find("(lower(nome) like ?1 "
+					+ "or lower(nome) like ?1) and status <> ?2",
+					"%" + termo.toLowerCase() + "%",
+					Status.INATIVO).fetch();
+		}
+		
+		render(listaDeProdutos, termo);
+		
+		
 	}
 	
 	public static void remover(Long id) {
 		Produto produto = Produto.findById(id);
-		produto.delete();
-		listar();
+		produto.status = Status.INATIVO;
+		produto.save();
+		listar(null);
 	}
 	
 	public static void editar(Long id) {
--- app/model/Produto.java
+++ app/model/Produto.java
@@ -1,6 +1,8 @@
 package model;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.ManyToOne;
 
 import play.db.jpa.Model;
@@ -10,7 +12,17 @@ public class Produto extends Model {
 
 	public String nome;
 	public int preco;
+	
+	//produto ativo e inativo
+	@Enumerated(EnumType.STRING)
+	public Status status;
+	
+	public Produto() {
+		this.status = Status.ATIVO;
+	}
 
+	
+	//Relacionamento produto-categoria 
 	@ManyToOne
 	public Categoria categoria;
 
