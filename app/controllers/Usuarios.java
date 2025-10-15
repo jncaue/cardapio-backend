db9984b95b7416beb358fd23fcced87dea6544e4
Gustavo Targino <luizgustavotargino23004@gmail.com>
2025-10-15 20:16:50 -0300

    validação dos campos do cadastro

--- app/controllers/Usuarios.java
+++ app/controllers/Usuarios.java
@@ -3,40 +3,37 @@ package controllers;
 import java.util.List;
 
 import model.Usuario;
+import play.data.validation.Valid;
 import play.mvc.Controller;
 import play.mvc.With;
 
 @With(Seguranca.class)
 public class Usuarios extends Controller {
-
 	public static void form() {
 		render();
 	}
-
-	public static void salvar(Usuario usuario, String senha) {
-
-		if (senha.equals("") == false) {
-			usuario.senha = senha;
+	public static void salvar(@Valid Usuario usuario) {
+		if (validation.hasErrors()) {
+			params.flash();
+			validation.keep();
+			form();
 		}
-
 		usuario.save();
 		Login.form();
 		flash.success("Logado com sucesso!");
 	}
-
 	public static void editar(Long id) {
 		Usuario usuario = Usuario.findById(id);
 		renderTemplate("Usuarios/form.html", usuario);
 	}
-
 	public static void remover(Long id) {
 		Usuario usuario = Usuario.findById(id);
 		usuario.delete();
 		listar();
 	}
-
 	public static void listar() {
 		List<Usuario> usuarios = Usuario.findAll();
 		render(usuarios);
 	}
 }
+
--- app/model/Usuario.java
+++ app/model/Usuario.java
@@ -4,19 +4,29 @@ import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 
+import play.data.validation.MinSize;
+import play.data.validation.Required;
 import play.db.jpa.Model;
 import play.libs.Crypto;
 
 @Entity
 public class Usuario extends Model{
 	
+	@Required
 	public String nome;
+	
+	@Required
 	public String matricula;
+	
+	@MinSize(4)
+	@Required
 	public String senha;
 	
-	public void setSenha(String s){
-		senha = Crypto.passwordHash(s);
+	public void Gerasenha(String Gerasenha){
+		senha = Crypto.passwordHash(Gerasenha);
 	}
+	
+	public Usuario usuario;
 	
 	public Usuario (String nome, String matricula, String senha) {
 		this.nome = nome;
