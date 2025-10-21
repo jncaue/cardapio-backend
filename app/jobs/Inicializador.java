f85eb55816dcabaee13fde6c17aad809c2284e04
jncaue <juancaue2016@gmail.com>
2025-10-21 08:51:54 -0300

    *

--- app/jobs/Inicializador.java
+++ app/jobs/Inicializador.java
@@ -1,5 +1,6 @@
 package jobs;
 
+import controllers.Tamanhos;
 import model.Categoria;
 import model.Perfil;
 import model.Produto;
@@ -18,80 +19,81 @@ public class Inicializador extends Job {
 		if (Categoria.count() == 0) {
 			Categoria comida = new Categoria("Comida");
 			comida.save();
-			
+
 			Categoria pizza = new Categoria("Pizza");
 			pizza.save();
-			
+
 			Categoria hamburguer = new Categoria("Hamburguer");
 			hamburguer.save();
-			
+
 			Categoria cachorroQuente = new Categoria("Cachorro quente");
 			cachorroQuente.save();
 
 			Categoria bebida = new Categoria("Bebida");
 			bebida.save();
-			
+
 			Categoria suco = new Categoria("Suco");
 			suco.save();
 
 			Categoria sobremesa = new Categoria("Sobremesa");
 			sobremesa.save();
-			
 
-//			inicia dois produtos para auxilio de execucao do site
-			if (Produto.count() == 0) {
-				Produto pepsi = new Produto();
-				pepsi.nome = "Pepsi";
-				pepsi.preco = 12;
-				pepsi.descricao= "Refrigerante";
-				pepsi.categoria = bebida;
-				pepsi.save();
+			// tamanhos dos produtos
+			if (Tamanho.count() == 0) {
 
-				Produto xbacon = new Produto();
-				xbacon.nome = "Xbacon ";
-				xbacon.preco = 20;
-				xbacon.descricao= "Hamburguer 90g, bacon, cheddar, salada";
-				xbacon.categoria = hamburguer;
-				xbacon.save();
-				
-				Produto açai = new Produto();
-				açai.nome = "Açaí";
-				açai.preco = 22;
-				açai.descricao= "Açaí 500g, banana, leite em pó, granola";
-				açai.categoria = sobremesa;
-				açai.save();
-				
-//				inicia um usuario raiz para acesso ao site
-				if (Usuario.count() == 0) {
+				Tamanho p = new Tamanho("P");
+				p.save();
 
-					Usuario Root = new Usuario("root", "1", "1111");
-					Root.perfil = Perfil.ADMINISTRADOR;
-					Root.save();
+				Tamanho m = new Tamanho("M");
+				m.save();
 
-					Usuario Admin = new Usuario("admin", "123123", "1234");
-					Admin.perfil = Perfil.ADMINISTRADOR;
-					Admin.save();
+				Tamanho g = new Tamanho("G");
+				g.save();
 
-					Usuario Cliente = new Usuario("cliente", "12312", "1234");
-					Cliente.perfil = Perfil.CLIENTE;
-					Cliente.save();
-					
-					if (Tamanho.count() == 0) {
+//			inicia produtos para auxilio de execucao do site
+				if (Produto.count() == 0) {
+					Produto pepsi = new Produto();
+					pepsi.nome = "Pepsi";
+					pepsi.preco = 12;
+					pepsi.tamanho = m;
+					pepsi.descricao = "Refrigerante";
+					pepsi.categoria = bebida;
+					pepsi.save();
 
-						Tamanho m = new Tamanho ("P");
-						m.save();
+					Produto xbacon = new Produto();
+					xbacon.nome = "Xbacon ";
+					xbacon.preco = 20;
+					xbacon.tamanho = m;
+					xbacon.descricao = "Hamburguer 90g, bacon, cheddar, salada";
+					xbacon.categoria = comida;
+					xbacon.save();
 
-						Tamanho p = new Tamanho ("M");
-						p.save();
-						
-						Tamanho g = new Tamanho ("G");
-						g.save();
+					Produto açai = new Produto();
+					açai.nome = "Açaí";
+					açai.preco = 22;
+					açai.tamanho = g;
+					açai.descricao = "Açaí 500g, banana, leite em pó, granola";
+					açai.categoria = sobremesa;
+					açai.save();
+
+//				inicia usuarios 
+					if (Usuario.count() == 0) {
+
+						Usuario Root = new Usuario("root", "1", "1111");
+						Root.perfil = Perfil.ADMINISTRADOR;
+						Root.save();
+
+						Usuario Admin = new Usuario("admin", "123123", "1234");
+						Admin.perfil = Perfil.ADMINISTRADOR;
+						Admin.save();
+
+						Usuario Cliente = new Usuario("cliente", "12312", "1234");
+						Cliente.perfil = Perfil.CLIENTE;
+						Cliente.save();
+
 					}
 				}
-
 			}
-
 		}
-
 	}
 }
